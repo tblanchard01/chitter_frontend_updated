f094cba35199fe45acccfbff747ca35ef76272c9
Timothy Blanchard <tim@Timothys-MacBook-Pro.local>
2019-11-23 17:12:21 +0000

    peeps showing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { Peep } from "./components/Peep/Peep";
+
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {peeps: {name: "tim", body: "hello"}}
-  
+  state = { peeps: [] };
+
+  componentDidMount() {
+    axios.get(`https://chitter-backend-api.herokuapp.com/peeps`).then(res => {
+      const peeps = res.data;
+      this.setState({ peeps });
+      console.log(this.state);
+    });
   }
+// todo: move this to a container component 
+
   render() {
-    const {name} = this.state.peeps 
     return (
-     <div>
-       hello {name}
-      </div>
+      <div>
+        <h1>hello</h1>
+        {this.state.peeps.map((peep, index) => {
+         const {user, body, likes} = peep; 
+         return  <Peep
+            key={index}
+            handle={user.handle}
+            body={body}
+            likes={likes.length}
+          />
+
+        })
+       } 
+       </div>
+
     );
-  }
+}
 }
 
 export default App;
