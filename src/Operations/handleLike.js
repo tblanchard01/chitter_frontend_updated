a0b6f25efca154d4a5d1a93fd25dd14e67078695
Tim <tim.blanchard@hotmail.co.uk>
2020-01-06 20:26:01 +0000

    likes button now working

--- src/Operations/handleLike.js
+++ src/Operations/handleLike.js
@@ -3,15 +3,15 @@ import axios from "axios";
 export function handleLike(userID, sessionKey, fetchPeeps) {
   return async function(peepId) {
     const res = await axios.put(
-      `https://chitter-backend-api.herokuapp.com/peeps/${peepId}/likes/${userID}`,
-      
+      `https://chitter-backend-api.herokuapp.com/peeps/${peepId}/likes/${userID}`,null,
       { headers: { authorization: `Token token=${sessionKey}` } }
     );
     if (res.status < 300) {
       fetchPeeps();
     } else {
     }
-  }};
+  };
+}
 
 //   /peeps/:peep_id/likes/:user_id
 
