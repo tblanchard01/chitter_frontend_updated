4cff157cb0b157a91e203f23fafaab59862304f9
Tim <tim.blanchard@hotmail.co.uk>
2020-01-12 14:49:42 +0000

    working

--- src/components/LikePeep/LikePeep.js
+++ src/components/LikePeep/LikePeep.js
@@ -1,29 +1,36 @@
-import  React, {useState } from "react";
+import React, { useState } from "react";
 import UnfilledHeart from "../../Images/Favourite/unfilled_heart.png";
-import FilledHeart from "../../Images/Favourite/unfilled_heart.png";
+import FilledHeart from "../../Images/Favourite/filled_heart.png";
 
 export function LikePeep({ id, authenticated, handleLike }) {
- const [liked, updateLikeState] = useState(false);
+  const [liked, updateLikeState] = useState(false);
 
- function like() {
-    handleLike(id);
-     updateLikeState(true);
-    console.log("....called like");
-    console.log({ liked });
+  function like(){
+    handleLike(id)
+    updateLikeState(true)
   }
 
   if (!liked && !authenticated) {
-    return <div> <img src={UnfilledHeart} alt="Like" /></div>
-    
+    return (
+      <div>
+        <img src={UnfilledHeart} alt="Like" />{" "}
+      </div>
+    );
   }
 
   if (!liked && authenticated) {
     return (
-      <div onClick={like}>
-        <img src={UnfilledHeart} alt="Like" />
+      <div onClick = {like}> 
+        <img src={UnfilledHeart} alt="Like" />{" "}
       </div>
     );
-  }}
+  }
+
+  if(liked){
+    return <div>'foo'</div>
+  }
+}
+
 
 
 // unliked && unathenticated => empty heart which does nothing (or displays login prompt)
